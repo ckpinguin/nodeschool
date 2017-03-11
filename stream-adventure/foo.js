d7da70bef4fa45425b7afa3f1a1ae55f3586276a
Christof Kälin <christof.kaelin@gmail.com>
2017-03-11 12:36:34 +0100

    initial, stream-adventure

---
+++ stream-adventure/foo.js
@@ -0,0 +1,10 @@
+
+function foo() {
+    var bar;
+    quux = 0;
+    function zip() {
+        var quux;
+        bar = true;
+    }
+    return zip;
+}
---
+++ stream-adventure/lines.js
@@ -0,0 +1,34 @@
+var through = require('through');
+var split = require('split');
+
+// Well, I cheated here, I got this solution from
+// https://github.com/jeremy-w/node-stream-adventure-solutions/blob/master/lines.js
+// There was no obvious way to use variables to check for modulo 2 or so...
+
+// "Give me 2 functions and I return a function, that calls one of them and
+// then next time the other etc."
+const alternate_fn = function(odd_fn, even_fn) {
+    // we start with even (at 0)
+    let f = even_fn; // closed over by returned anon. function below
+    return function(buffer) {
+        f = (f === even_fn) ? odd_fn : even_fn;
+        f.call(this, buffer); // `this` will be the string on which f is called
+    };
+};
+
+// "Give me a string function and I will return a function, that works on a
+// buffer calling the string function on that buffer and putting the
+// changed string onto the stream"
+const enqueue_after = function(string_fn) {
+    return function(buffer) {
+        const value = string_fn.call(buffer.toString()) + '\n';
+        this.queue(value); // This only works with through, not with through2
+    };
+};
+
+process.stdin
+    .pipe(split())
+    .pipe(through(alternate_fn(
+        enqueue_after(String.prototype.toLowerCase),
+        enqueue_after(String.prototype.toUpperCase))))
+    .pipe(process.stdout);
